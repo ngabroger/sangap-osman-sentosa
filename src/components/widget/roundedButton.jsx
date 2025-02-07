465001b4e4f6c857bf7aaf2ca5b84719f4f51b9a
ngabroger <arbination13@gmail.com>
2025-02-07 22:22:58 +0700

    fix for detail page

--- src/components/widget/roundedButton.jsx
+++ src/components/widget/roundedButton.jsx
@@ -1,10 +1,10 @@
-export default function RoundedButton({link}){
-
-    return (
-        <a href={link}>
-        <button className="arrow-button  bg-gray-900 text-white rounded-full w-12 h-12  items-center justify-center hover:bg-gray-800">
-                  ➜
-        </button>
-        </a>
-    );
+import { Link } from 'react-router-dom';
+export default function RoundedButton({ link }) {
+  return (
+    <Link to={link}>
+      <button className="arrow-button  bg-gray-900 text-white rounded-full w-12 h-12  items-center justify-center hover:bg-gray-800">
+        ➜
+      </button>
+    </Link>
+  );
 }
